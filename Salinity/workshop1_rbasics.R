f34a8f5479e6245d9124f246da9847e12487b182
M. C. Randall <Rand0137@flinders.edu.au>
2025-07-03 13:22:07 +0930

    2024 salinity data - Maddi still to edit code

---
+++ Salinity/workshop1_rbasics.R
@@ -0,0 +1,271 @@
+# ******************************************************************************
+###                  BIOL3712,  Flinders University, SA                      ###
+###                     Introduction to R programming                        ###
+###           By Sofie Costin, based on material from Rex Mitchell           ###
+### https://www.youtube.com/playlist?list=PL42DD6-0IeVOc4rnMKwAcx6zCww5vzjV_ ###
+###                     Last updated 04/09/2024                             ###
+# ******************************************************************************
+
+# Introduction to R -------------------------------------------------------
+
+# First thing's first.The hash symbol '#' is known as the 'commenting symbol'.
+# It is used to make notes that are not recognized as code by the software. 
+
+# You are reading this code in Rstudio. Rstudio is NOT R! Rstudio is an editing
+# software that sends code you've written to R for executing. You can see the R 
+# console below. Using Rstudio, you can slowly build your analyses and re-run 
+# them as often as you like. If something does not work, you can fiddle with 
+# it in Rstudio and send it back to R.
+
+# selecting one/many line/s and hitting ctrl/cmd + enter/return will run the selected code
+# (but it won't run anything that has a #-symbol at the start).
+
+# For every analysis, you should begin a new R script and save it to a file that
+# contains your data. This means that when you import and save files and images,
+# they will all be contained within the same folder.
+
+
+
+### Basic syntax ###
+
+# R can be used as a calculator, similar to excel.
+# '+' is plus
+# '-' is minus
+# '/' is division
+# '*' is multiplication
+# '^' is exponent
+# 'sqrt' is the square root
+# There's others too, some of which we'll get to in these sessions.
+
+# Example:
+1+1
+2^2
+sqrt(4)
+# Notice the solution is presented in the Console panel below.
+
+# Workbook question 1a: --------
+# please write and run three lines of code to answer three simple maths 
+# problems (like the examples above)
+
+# The arrow '<-' is called the 'Assignment Operator' in R
+# It is used to assign information to an object.
+
+# You can name the object anything you like. For example, let''s create an object called 'dirty socks', 
+# and store a simple math problem inside.
+dirty_socks <- 2+2
+# Notice that the object appears in the Environment window (top right).
+
+# If you type the object alone, the information it contains is presented in the 
+# console, in this case a vector with a single value.
+dirty_socks
+
+# You can then call upon that allocated information in further 
+# calculations/analyses
+dirty_socks*15
+# Notice that an answer of 60 is printed in the console.
+# In theory, this could continue ad infinitum. I could allocate the above line 
+# to a new object:
+clean_shoes <- dirty_socks*15
+# And now the environment contains this object as well for future use.
+
+# This can dramatically shorten coding for later usage
+
+### TWO NOTES ON THIS ###
+# 1) R is case-sensitive! Be careful!
+# 2) No spaces allowed in object names between words. Instead use '_' or '.'
+#### In our lab, we use '_' for objects and '.' for functions!!!
+
+
+### Functions ###
+
+# A function is a piece of code written to carry out a specified task.
+# They are made for our convenience by other researchers. 
+# Bless their cotton socks :D
+
+# For example, if you want to print something in your console, you can use the cat function. 
+cat("I would rather wear no socks than dirty socks")
+
+
+# Workbook question 1b: ---------------------------------------------------
+# tell me what your name is!
+
+# create an object for your name:
+myname <- "sofie costin" # enter your name here instead of my name
+cat("************ My name is", myname, "************")
+
+
+
+### Data Formats (see reference image) ###
+
+## A vector ##
+
+# A vector: 1 or more numbers in a 1-dimensional row of all the same data type
+# Lets make a vector of numbers 1 through 18. We are going to call it 'vec' - short for vector.
+
+vec <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)
+# The 'c' stands for concatenate or combine. It creates a temporary list for
+# assignment.
+# Notice that 'vec' has appeared in the environment to the right.
+# If you run the object as code, the vector will be presented in the console.
+vec
+# Instead of writing all numbers, you can shorten this by just separating the 
+# maximum and minimum values by a colon ':'
+vec2 <- c(1:18)
+vec2
+# (Note: the difference in the objects in the environment. One is class integer,
+# the other is class numeric. This doesn't matter, as R automatically converts 
+# between numeric classes.)
+
+
+# Workbook question 1c: ----------------------------------------------------
+# create a vector using 16 numbers of your choice. you can call it anything you like
+
+
+
+
+## A matrix ##
+
+# A matrix consists of rows and columns. A 2-dimensional array. 
+# Requires the same length of rows and same data type.
+# Let's make a matrix this time, using the same numbers, but divided into 3 rows.
+# This time we use the 'matrix' function
+?matrix
+# We'll name this one 'mat'- short for matrix
+
+#Here, we distribute the vec vector (1:18) across three rows (i.e. 3 rows with 6 columns each = 18)
+mat <- matrix(c(1:18), nrow=3)
+mat
+# Because the 'vec' vector is a vector from 1:18, the above is the same as:
+mat2 <- matrix(vec, nrow=3)
+mat2
+# Notice how the numbers have been distributed by columns? 
+
+
+# Workbook question 1d: ---------------------------------------------------
+# turn your vector into a matrix with four rows
+
+
+
+
+## A dataframe ##
+
+# A dataframe is similar to a matrix, in that it consists of rows and columns. 
+# The key difference is, that a dataframe can include data of different types. 
+# we can use the as.data.frame function to turn our 'mat' matrix into a dataframe
+mat_df <- as.data.frame(mat)
+# if we compare our mat matrix with our mat data frame, we can notice a few differences.
+mat
+mat_df
+# we can then add a column of characters (called "new") to our mat_df
+mat_df$new <- c('one', 'two', 'three')
+mat_df
+# and we can rename one of our column headers (here, we use column 1 by typing [1], 
+#but it could be column 2 if you typed [2])
+names(mat_df)[1] <- 'case'
+mat_df
+
+# check out our new dataframe:
+str(mat_df) # shows us our column labels, our data types, and the first few data points.
+
+
+
+# Workbook question 1e:  --------------------------------------------------
+# turn your matrix into a dataframe. add a column called 'pie' of characters (words) that relate to pies.
+# (this is equivalent to the addition of the 'mat_df$new' column)
+# rename another column to be called 'pizza'
+
+
+
+# You can clear the environment with the following function:
+rm(list = ls())
+# Alternatively, click on the broom icon above the environment window. 
+# The same icon is present in the console and plots windows.
+
+
+### Now lets look at some data!!! ###
+
+# R has many stored datasets
+# To view the list:
+data()
+
+# You can view the details of each dataset.
+?iris # for example
+
+# We can allocate this data to an object, so that it appears in the environment
+data <- iris
+
+# The data can be viewed either by clicking on the small table icon to the right
+# of the object in the environment, or by run the object as code.
+data
+
+# The function 'head' produces the first six lines of the dataset.
+head(iris) 
+
+# The 'summary' function presents the main parameters of the data.
+summary(iris) 
+# prints number of individuals, variable means, sds, IQR, etc
+# NOTE: the data is essentially a matrix by coordinates. SO we can extract any
+# data via their coordinates, just like before!
+# for exammple, data[1,2] will give us the first value of the second column (sepal width)
+data[1,2]
+
+
+### Now lets do some extracting! ###
+
+# The dollar sign '$' is used to extract named elements from a named list. 
+# You can use this to extract columns from a dataframe, which is what we will do 
+# later down the track.
+
+# For example, we can allocate the variable of sepal length to its own object:
+sepalL <- data$Sepal.Length
+sepalL 
+# Sepal length is now a vector with 150 entries - check this in the environment
+
+# Lets do this for all variables. It's not necessary, but it shortens lines of code.
+sepalW <- data$Sepal.Width
+petalL <- data$Petal.Length
+petalW <- data$Petal.Width
+species <- data$Species
+# Note that species is qualitative
+species
+# So it is registered as a Factor with 150 observations across three levels.
+
+# if you save your workspace at the end of your session, it will save all of the 
+# information in your global environment, which is especially handy when your code 
+# takes a long time to run.
+
+
+
+print("congratulations, now it's time to start your salinity analysis!")
+
+
+
+######~~~~~~~Some additional info for the nerds among you :-D ~~~~~~######
+
+# These functions are stored in packages
+
+### Packages ###
+
+# Some are automatically loaded when you start R. For example, 'stats'
+# To view a package's contents (this will open a new window, but 
+#this code is on the tab to the left):
+library(help = "stats")
+# This will open a new window listing all of the details of the package.
+# This should include the authors for citations and all functions to search.
+# When you find a function that you wish to use, you can search that function
+# in the help box (bottom right window, 4th tab) and see exactly how to use it.
+# Alternatively, type '?' followed by the function.
+# For example, lets look at the function for a linear model, 'lm'
+?lm
+
+# Two other types of packages:
+# 1) Base packages: installed with R but are not automatically loaded to save 
+# memory. (see list in bottom right window, 3rd tab)
+# 2) Contributed packages: created by external parties. Must be installed and 
+# loaded. For example, 'geomorph' is specifically tailored for Geometric 
+# Morphometrics. This must be installed.
+
+# Three places to find them:
+# 1)	Search in the packages window (click install and type the package name)
+# 2)	CRAN: comprehensive R archive network. Go to https://cran.r-project.org/
+# 3)	GitHub: https://github.com/trending/r
---
+++ Salinity/workshop1_salinitycont_2024.R
@@ -0,0 +1,421 @@
+# ******************************************************************************
+
+###                     Rex and Sof's Salinity Workshop                      ###
+
+# ******************************************************************************
+
+# ******************************************************************************
+
+# Salinity Data Analysis continued ----------------
+
+# ******************************************************************************
+
+### PLEASE NOTE THAT USING THIS CODE WILL VOID YOUR CHANCE AT GETTING AN HD FOR 
+### THIS PART OF THE ASSIGNMENT
+
+# first, we're going to clear the data from our global environment, so we have a 
+# fresh start
+
+rm(list = ls())
+
+# and load our libraries
+library(ggpubr)
+library(dplyr)
+library(car)
+
+# Let's import our data
+
+all_data <- na.omit(data.frame(read.csv("Salinity_data.csv")))  
+# read.csv will import the data from our working directory
+head(all_data) 
+# note that we now have all_data in our global environment, which is 108 observations 
+# of 20 variables
+# the name of our species column is a little weird, so lets sort that out:
+names(all_data)[1] <- 'species'
+# and we can check that everything looks as we expect it to:
+str(all_data)
+
+
+# Let's assign all of the variables we're going to use to their own object. 
+species <- all_data$species
+saltConc <- as.character(all_data$salt_conc) 
+fwShootRoot <- all_data$fw_shoot_root_ratio
+dwShootRoot <- all_data$dw_shoot_root_ratio
+shootMoist <- all_data$shoot_moisture
+rootMoist <- all_data$root_moisture
+totMoist <- all_data$total_moisture
+
+alldata_df <- cbind.data.frame(species, saltConc, fwShootRoot, dwShootRoot, 
+                               shootMoist, rootMoist, totMoist)
+
+# check all is as we expect:
+str(alldata_df)
+
+# Fresh weight root to shoot ratio ----------------------------------
+
+### Assumption 1: data are normally distributed
+shapiro.test(alldata_df$fwShootRoot)
+
+hist(alldata_df$fwShootRoot, breaks = 100)
+
+# woah, something whacky is happening here. Let's open our data and have a look
+# what can you see is wrong? we need to remove some outliers
+
+# first, we work out what our first and third quartiles are for our data:
+quartiles <- quantile(alldata_df$fwShootRoot, probs=c(.25, .75), na.rm = FALSE) 
+# and determine the interquartile range (IQR)
+IQR <- IQR(alldata_df$fwShootRoot)
+
+# Then we find our lower and upper limits:
+Lower <- quartiles[1] - 1.5*IQR
+Upper <- quartiles[2] + 1.5*IQR 
+
+# and reassign any data above the lower threshold and below the upper threshold to a new dataframe, 
+# which we will use for the analysis.
+fwShootRoot_noOutliers <- filter(alldata_df, fwShootRoot > Lower & fwShootRoot < Upper )
+
+#There are still some clearly horrible data in here. Let's take them out. 
+
+# After relevant outliers are removed, we can then re-test for normality using our Shapiro-Wilk tests.
+shapiro.test(fwShootRoot_noOutliers$fwShootRoot)
+
+# much better
+hist(fwShootRoot_noOutliers$fwShootRoot)
+
+ggboxplot(fwShootRoot_noOutliers, x = "saltConc", y = "fwShootRoot", color = "species")
+
+#This is still not great. Normally, we would go thorugh and remove the offending data in the dataset, but this will be too confusing. But again, some of the plants have been shooting atypically fast, so it's OK to take out any values that are very high, e.g. higher than 1.5:
+fwShootRoot_noOutliers <- filter(alldata_df, fwShootRoot < 1.6 )
+
+# Re-test for normality using our Shapiro-Wilk tests.
+shapiro.test(fwShootRoot_noOutliers$fwShootRoot)
+
+# much better
+hist(fwShootRoot_noOutliers$fwShootRoot)
+
+ggboxplot(fwShootRoot_noOutliers, x = "saltConc", y = "fwShootRoot", color = "species")
+
+
+table(fwShootRoot_noOutliers$species, fwShootRoot_noOutliers$saltConc)
+
+# as good as it gets. Let's proceed.
+
+### Assumption 2: variances are homogeneous
+
+library(car) # access the car package
+
+leveneTest(fwShootRoot ~ species*saltConc, data = fwShootRoot_noOutliers)
+
+### two-way ANOVA
+
+fwShootRoot.aov <- aov(fwShootRoot ~ species * saltConc, data = fwShootRoot_noOutliers)
+summary(fwShootRoot.aov)
+#have a good look at this. What is this telling us - what follow-on tests are appropriate, which are not?
+
+# we can generate some summary statistics for our response data, depending on our two factors:
+require("dplyr")
+group_by(fwShootRoot_noOutliers, species, saltConc) %>%
+  summarise(count = n(),mean = mean(fwShootRoot, na.rm = TRUE),
+            sd = sd(fwShootRoot, na.rm = TRUE))
+
+# post-hoc test
+
+TukeyHSD(fwShootRoot.aov, which = "saltConc")
+# when interpreting the output, diff: difference between means of the two groups, 
+# lwr & upr: the lower and the upper end point of the confidence interval at 95%, 
+# p adj: p-value after adjustment for the multiple comparisons.
+
+
+### let's graph it!
+
+ggline(fwShootRoot_noOutliers, x = "saltConc", y = "fwShootRoot", color = "species",
+       add = c("mean_se"),
+       xlab = "salt concentration (g/L)",
+       ylab = "fresh weight shoot to root ratio (%)",
+       legend = "right")
+
+# once we're happy with our plot, we click "export" and save it in our working folder. 
+
+
+# Dry weight root to shoot ratio ----------------------------------
+
+### Assumption 1: data are normally distributed
+shapiro.test(alldata_df$dwShootRoot)
+
+hist(alldata_df$dwShootRoot)
+
+ggboxplot(alldata_df, x = "saltConc", y = "dwShootRoot", color = "species")
+
+# remove outliers
+
+# first, we work out what our first and third quartiles are for our data:
+quartiles <- quantile(alldata_df$dwShootRoot, probs=c(.25, .75), na.rm = FALSE) 
+# and determine the interquartile range (IQR)
+IQR <- IQR(alldata_df$dwShootRoot)
+
+# Then we find our lower and upper limits:
+Lower <- quartiles[1] - 1.5*IQR
+Upper <- quartiles[2] + 1.5*IQR 
+
+# and reassign any data above the lower threshold and below the upper threshold to a new dataframe, 
+# which we will use for the analysis.
+dwShootRoot_noOutliers <- filter(alldata_df, dwShootRoot > Lower & dwShootRoot < Upper )
+
+# After relevant outliers are removed, we can then re-test for normality using our Shapiro-Wilk tests.
+shapiro.test(dwShootRoot_noOutliers$dwShootRoot)
+
+# much better
+hist(dwShootRoot_noOutliers$dwShootRoot)
+
+ggboxplot(dwShootRoot_noOutliers, x = "saltConc", y = "dwShootRoot", color = "species")
+
+#again, we have some interesting differences possibly due to anomalous plants, but we will assume that the ANOVA is robust to thes few outliers.
+
+table(dwShootRoot_noOutliers$species, dwShootRoot_noOutliers$saltConc)
+
+
+### Assumption 2: variances are homogeneous
+
+leveneTest(dwShootRoot ~ species*saltConc, data = dwShootRoot_noOutliers)
+
+### two-way ANOVA
+
+dwShootRoot.aov <- aov(dwShootRoot ~ species * saltConc, data = dwShootRoot_noOutliers)
+summary(dwShootRoot.aov)
+
+# we can generate some summary statistics for our response data, depending on our two factors:
+
+group_by(dwShootRoot_noOutliers, species, saltConc) %>%
+  summarise(count = n(),mean = mean(dwShootRoot, na.rm = TRUE),
+            sd = sd(dwShootRoot, na.rm = TRUE))
+
+# post-hoc test
+
+TukeyHSD(dwShootRoot.aov, which = "saltConc")
+# when interpreting the output, diff: difference between means of the two groups, 
+# lwr & upr: the lower and the upper end point of the confidence interval at 95%, 
+# p adj: p-value after adjustment for the multiple comparisons.
+
+
+### let's graph it!
+
+ggline(dwShootRoot_noOutliers, x = "saltConc", y = "dwShootRoot", color = "species",
+       add = c("mean_se"),
+       xlab = "salt concentration (g/L)",
+       ylab = "dry weight shoot to root ratio (%)",
+       legend = "right")
+
+# once we're happy with our plot, we click "export" and save it in our working folder.
+
+
+# Shoot Moisture ----------------------------------
+
+### Assumption 1: data are normally distributed
+shapiro.test(alldata_df$shootMoist)
+
+hist(alldata_df$shootMoist)
+
+ggboxplot(alldata_df, x = "saltConc", y = "shootMoist", color = "species")
+
+# remove outliers
+
+# first, we work out what our first and third quartiles are for our data:
+quartiles <- quantile(alldata_df$shootMoist, probs=c(.25, .75), na.rm = FALSE) 
+# and determine the interquartile range (IQR)
+IQR <- IQR(alldata_df$shootMoist)
+
+# Then we find our lower and upper limits:
+Lower <- quartiles[1] - 1.5*IQR
+Upper <- quartiles[2] + 1.5*IQR 
+
+# and reassign any data above the lower threshold and below the upper threshold to a new dataframe, 
+# which we will use for the analysis.
+shootMoist_noOutliers <- filter(alldata_df, shootMoist > Lower & shootMoist < Upper )
+
+# After relevant outliers are removed, we can then re-test for normality using our Shapiro-Wilk tests.
+shapiro.test(shootMoist_noOutliers$shootMoist)
+
+# much better
+hist(shootMoist_noOutliers$shootMoist)
+
+ggboxplot(shootMoist_noOutliers, x = "saltConc", y = "shootMoist", color = "species")
+
+table(shootMoist_noOutliers$species, shootMoist_noOutliers$saltConc)
+
+
+### Assumption 2: variances are homogeneous
+
+library(car) # access the car package
+
+#What does the result mean? We will continue here and assume the ANOVA is robust, but you will be able to see why this test is significant when you look at the ggboxplot above.
+leveneTest(shootMoist ~ species*saltConc, data = shootMoist_noOutliers)
+
+
+### two-way ANOVA
+
+shootMoist.aov <- aov(shootMoist ~ species * saltConc, data = shootMoist_noOutliers)
+summary(shootMoist.aov)
+
+# summary statistics
+group_by(shootMoist_noOutliers, species, saltConc) %>%
+  summarise(count = n(),mean = mean(shootMoist, na.rm = TRUE),
+            sd = sd(shootMoist, na.rm = TRUE))
+
+# post-hoc test
+
+TukeyHSD(shootMoist.aov, which = "saltConc")
+# when interpreting the output, diff: difference between means of the two groups, 
+# lwr & upr: the lower and the upper end point of the confidence interval at 95%, 
+# p adj: p-value after adjustment for the multiple comparisons.
+
+
+### let's graph it!
+
+ggline(shootMoist_noOutliers, x = "saltConc", y = "shootMoist", color = "species",
+       add = c("mean_se"),
+       xlab = "salt concentration (g/L)",
+       ylab = "dry weight shoot to root ratio (%)",
+       legend = "right")
+
+# once we're happy with our plot, we click "export" and save it in our working folder.
+
+# Root Moisture ----------------------------------
+
+### Assumption 1: data are normally distributed
+shapiro.test(alldata_df$rootMoist)
+
+hist(alldata_df$rootMoist)
+
+ggboxplot(alldata_df, x = "saltConc", y = "rootMoist", color = "species")
+
+# remove outliers
+
+# first, we work out what our first and third quartiles are for our data:
+quartiles <- quantile(alldata_df$rootMoist, probs=c(.25, .75), na.rm = FALSE) 
+# and determine the interquartile range (IQR)
+IQR <- IQR(alldata_df$rootMoist)
+
+# Then we find our lower and upper limits:
+Lower <- quartiles[1] - 1.5*IQR
+Upper <- quartiles[2] + 1.5*IQR 
+
+# and reassign any data above the lower threshold and below the upper threshold to a new dataframe, 
+# which we will use for the analysis.
+rootMoist_noOutliers <- filter(alldata_df, rootMoist > Lower & rootMoist < Upper )
+
+# After relevant outliers are removed, we can then re-test for normality using our Shapiro-Wilk tests.
+shapiro.test(rootMoist_noOutliers$rootMoist)
+
+# much better
+hist(rootMoist_noOutliers$rootMoist)
+
+ggboxplot(rootMoist_noOutliers, x = "saltConc", y = "rootMoist", color = "species")
+
+table(rootMoist_noOutliers$species, rootMoist_noOutliers$saltConc)
+
+
+### Assumption 2: variances are homogeneous
+
+leveneTest(rootMoist ~ species*saltConc, data = rootMoist_noOutliers)
+
+#Both Shapiro or Levene test are significant - so again, this needs flagging in your report. Can you think of why the variance in the lower concentrations is so much higher than in the upper ones?
+
+### two-way ANOVA
+
+rootMoist.aov <- aov(rootMoist ~ species * saltConc, data = rootMoist_noOutliers)
+summary(rootMoist.aov)
+
+# we can generate some summary statistics for our response data, depending on our two factors:
+
+group_by(rootMoist_noOutliers, species, saltConc) %>%
+  summarise(count = n(),mean = mean(rootMoist, na.rm = TRUE),
+            sd = sd(rootMoist, na.rm = TRUE))
+
+# post-hoc test
+
+TukeyHSD(rootMoist.aov, which = "saltConc")
+# when interpreting the output, diff: difference between means of the two groups, 
+# lwr & upr: the lower and the upper end point of the confidence interval at 95%, 
+# p adj: p-value after adjustment for the multiple comparisons.
+
+
+### let's graph it!
+
+ggline(rootMoist_noOutliers, x = "saltConc", y = "rootMoist", color = "species",
+       add = c("mean_se"),
+       xlab = "salt concentration (g/L)",
+       ylab = "dry weight shoot to root ratio (%)",
+       legend = "right")
+
+# once we're happy with our plot, we click "export" and save it in our working folder.
+
+# Total Moisture ----------------------------------
+
+### Assumption 1: data are normally distributed
+shapiro.test(alldata_df$totMoist)
+
+hist(alldata_df$totMoist)
+
+ggboxplot(alldata_df, x = "saltConc", y = "totMoist", color = "species")
+
+# remove outliers
+
+# first, we work out what our first and third quartiles are for our data:
+quartiles <- quantile(alldata_df$totMoist, probs=c(.25, .75), na.rm = FALSE) 
+# and determine the interquartile range (IQR)
+IQR <- IQR(alldata_df$totMoist)
+
+# Then we find our lower and upper limits:
+Lower <- quartiles[1] - 1.5*IQR
+Upper <- quartiles[2] + 1.5*IQR 
+
+# and reassign any data above the lower threshold and below the upper threshold to a new dataframe, 
+# which we will use for the analysis.
+totMoist_noOutliers <- filter(alldata_df, totMoist > Lower & totMoist < Upper )
+
+# After relevant outliers are removed, we can then re-test for normality using our Shapiro-Wilk tests.
+shapiro.test(totMoist_noOutliers$totMoist)
+
+# much better
+hist(totMoist_noOutliers$totMoist)
+
+ggboxplot(totMoist_noOutliers, x = "saltConc", y = "totMoist", color = "species")
+
+table(totMoist_noOutliers$species, totMoist_noOutliers$saltConc)
+
+
+### Assumption 2: variances are homogeneous
+
+leveneTest(totMoist ~ species*saltConc, data = totMoist_noOutliers)
+
+
+### two-way ANOVA
+
+totMoist.aov <- aov(totMoist ~ species * saltConc, data = totMoist_noOutliers)
+summary(totMoist.aov)
+
+# we can generate some summary statistics for our response data, depending on our two factors:
+require("dplyr")
+group_by(totMoist_noOutliers, species, saltConc) %>%
+  summarise(count = n(),mean = mean(totMoist, na.rm = TRUE),
+            sd = sd(totMoist, na.rm = TRUE))
+
+# post-hoc test
+
+TukeyHSD(totMoist.aov, which = "saltConc")
+# when interpreting the output, diff: difference between means of the two groups, 
+# lwr & upr: the lower and the upper end point of the confidence interval at 95%, 
+# p adj: p-value after adjustment for the multiple comparisons.
+
+
+### let's graph it!
+
+ggline(totMoist_noOutliers, x = "saltConc", y = "totMoist", color = "species",
+       add = c("mean_se"),
+       xlab = "salt concentration (g/L)",
+       ylab = "dry weight shoot to root ratio (%)",
+       legend = "right")
+
+# once we're happy with our plot, we click "export" and save it in our working folder.
+
+### well done!
